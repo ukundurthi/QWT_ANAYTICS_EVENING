8168f672532523631d658b6def31ee023d17424b
udaya bhaskar kundurthi <udayabhaskar.kundurthi@cognizant.com>
2025-02-01 08:53:43 +0000

    aa

---
+++ models/staging/customers2.sql
@@ -0,0 +1,14 @@
+create or replace table customers
+(
+CustomerID number,
+CompanyName varchar,
+ContactName varchar,
+City varchar,
+Country varchar,
+DivisionID number,
+Address varchar,
+Fax varchar,
+Phone varchar,
+PostalCode varchar,
+StateProvince varchar
+);
